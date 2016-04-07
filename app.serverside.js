5c77335752bf5839485876386197b3952fd83fe1
Natu Myers <me@natumyers.com>
2016-04-07 14:28:43 -0400

    update

---
+++ app.serverside.js
@@ -0,0 +1,7 @@
+var express = require('express')
+var app = express()
+
+
+        app.use(express.static(__dirname + "/clientside2")); app.get('/', function(req, res) {
+                    });
+app.listen(3111)
